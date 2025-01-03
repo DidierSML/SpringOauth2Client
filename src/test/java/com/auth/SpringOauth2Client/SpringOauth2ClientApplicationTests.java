33fa33b5591cc823f98ab86df77ab82528af858b
DidierSML <didiersmaldonad@gmail.com>
2025-01-03 17:57:36 -0500

    Implementation of Authentication with Oauth2 and Spring Security in a Spring Boot project

---
+++ src/test/java/com/auth/SpringOauth2Client/SpringOauth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.SpringOauth2Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
